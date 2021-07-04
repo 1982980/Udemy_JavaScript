b70e7be4113f0c17e4d8bf10aa562563a074c1b4
Роман Кононович <60620168+1982980@users.noreply.github.com>
2021-07-04 13:46:53 +0300

    new course files

---
+++ Lesson2/script.js
@@ -0,0 +1,19 @@
+// console.log("string" * 9);
+// const persone = "Alex";
+// console.log(persone);
+
+// console.log(smth);
+
+// let und;
+// console.log(und);
+
+const obj = {
+  name: "John",
+  age: 25,
+  isMarried: false,
+};
+// console.log(obj.name);
+console.log(obj["name"]);
+
+let arr = ["plum.pnp", "orange.png, {}"];
+console.log(arr[1]);
---
+++ Lesson3/script.js
@@ -0,0 +1,13 @@
+"use strict";
+// alert("Hello World");
+
+// const res = confirm("lorem?")
+
+const answers = [];
+// answers[0] = prompt("Ваше имя?", "");
+// answers[1] = prompt("Ваша фамилия?", "");
+// answers[2] = prompt("Ваш возраст?", "");
+
+// document.write(answers);
+console.log(typeof answers);
+console.log(typeof null);
