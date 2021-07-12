ade5787da1e54264b336a8abcf39f1a15f5441de
Roman <6417172@gmail.com>
2021-07-12 22:16:09 +0300

    Коммит домашнего задания, которое сделал сам, без исправлений

---
+++ Lesson13/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const arr = [1, 2, 3, 6, 8];
+arr.pop(); //удалил последний элемент массива "8"
+arr.push(10); //добавил в конец массива новый элемент "10"
+arr.shift(); //удалил первый элемент массива
+arr.unshift(9); //добавил в начало массива новый элемент "9"
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+// console.log(arr.length); //индекс последнего элемента массива +1
+arr.forEach(function (item, i, arr) {
+  console.log(`${i}: ${item} внутри массива ${arr}`);
+});
+
+// const str = prompt("?", "");
+// const product = str.split(", "); // м
+// product.sort(); //метод сортировки, который сортирует как строку
+// const productStr = product.join("; ");
+// console.log(productStr);
+
+const arrForSort = [2, 13, 26, 8, 10];
+// arrForSort.sort(compareNum);
+
+function compareNum(a, b) {
+  return a - b;
+}
+console.log(arrForSort.sort(compareNum)); //чтобы метод сортировал числа по возрастанию, нужно
+//использовать дополнительную функцию callback
