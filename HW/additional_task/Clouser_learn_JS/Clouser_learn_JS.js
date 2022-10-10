ce4e9666dfca7f2e2a65c703a864521ddd5a8ee4
Roman <6417172@gmail.com>
2022-10-10 23:16:00 +0300

    Домашенее задание. Последняя задача неочевидно сформулирована поэтому мое решение отличалось. Оставил ответ, который был в ответах

--- HW/additional_task/Clouser_learn_JS/Clouser_learn_JS.js
+++ HW/additional_task/Clouser_learn_JS/Clouser_learn_JS.js
@@ -265,3 +265,72 @@ console.log(army);
 //*! text
 //*TODO text
 // Пример: text;
+
+function test() {
+  console.log(a);
+  console.log(foo());
+
+  var a = 1;
+  function foo() {
+     return 2;
+  }
+}
+
+test();
+
+var a = 1;
+
+function someFunction(number) {
+  function otherFunction(input) {
+    return a;
+  }
+
+  a = 5;
+
+  return otherFunction;
+}
+
+var firstResult = someFunction(9);
+var result = firstResult(2);
+console.log(result);
+
+var fullname = 'John Doe';
+var obj = {
+   fullname: 'Colin Ihrig',
+   prop: {
+      fullname: 'Aurelio De Rosa',
+      getFullname: function() {
+         return this.fullname;
+      }
+   }
+};
+
+console.log(obj.prop.getFullname());
+
+var test = obj.prop.getFullname;
+
+console.log(test());
+
+
+var a = 10
+
+function bar() {
+  var a = 20
+
+  function foo(){
+    console.log(a)
+  }
+
+  foo()
+}
+
+bar()
+
+var a = 1;
+function b() {
+    a = 10;
+    return;
+    function a() {}
+}
+b();
+console.log(a);
